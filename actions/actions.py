1ca8e330bcfaec87108193c163cc0824a110e84a
Aww919 <andreaaiwen2000@gmail.com>
2022-06-06 23:59:01 +0200

    nuevas preguntas y action_nota

--- actions/actions.py
+++ actions/actions.py
@@ -74,7 +74,10 @@ class ActionPregPrincipiante(Action):
                             {"payload":resp2,"title":resp2},
                             {"payload":resp3,"title":resp3},
                         ])
-            return[SlotSet("n_pregunta",n_aleatorio)]
+            n_preguntas_seguidas=tracker.get_slot("preguntas_seguidas")
+            n_preguntas_seguidas=n_preguntas_seguidas+1
+
+            return[SlotSet("n_pregunta",n_aleatorio),SlotSet("preguntas_seguidas",n_preguntas_seguidas)]
 
 class ActionComprobarRespuesta(Action):
 
@@ -85,11 +88,13 @@ class ActionComprobarRespuesta(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
             n_preg_seleccionada=tracker.get_slot("n_pregunta")
+            numero_correctos=tracker.get_slot("n_correctos")
             resp_correcta=linecache.getline('resp_principiante.txt',n_preg_seleccionada)
 
             user_respuesta=str(tracker.latest_message['text'])
             n_linea_solucion=((n_preg_seleccionada-1)*5+1)+4
-            if user_respuesta in resp_correcta:
+            if user_respuesta.lower() in resp_correcta.lower():
+                numero_correctos=numero_correctos+1
                 dispatcher.utter_message(text="Enhorabuena, has elegido la respuesta correcta.")
             else:
                 solucion=linecache.getline('preg_principiante.txt',n_linea_solucion)
@@ -97,9 +102,36 @@ class ActionComprobarRespuesta(Action):
                 dispatcher.utter_message(text=solucion)
             '''dispatcher.utter_message(text=user_respuesta)
                 dispatcher.utter_message(text=resp_correcta)'''
-            return[] 
+            return[SlotSet("n_correctos",numero_correctos)] 
 
-    '''    
+class ActionNota(Action):
+
+    def name(self) -> Text:
+        return "action_nota"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            n_total_preg=tracker.get_slot("preguntas_seguidas")
+            n_resp_correctas=tracker.get_slot("n_correctos")
+
+            nota=n_resp_correctas/n_total_preg
+            dispatcher.utter_message(text=f"Has obtenido {n_resp_correctas} aciertos de {n_total_preg}.")
+            if nota == 1:
+                dispatcher.utter_message(text="Enhorabuena, ¡estás en racha de aciertos! ")
+            elif nota >= 0.5:
+                dispatcher.utter_message(text=f"Muy bien, has superado la prueba, ¡pero practica más para obtener un 100% de aciertos! ")
+            else:
+                dispatcher.utter_message(text="¡Oh no...no has superado la prueba, deberías de estudiar más. ")
+
+            return[SlotSet("n_pregunta",0),SlotSet("preguntas_seguidas",0),SlotSet("n_correctos",0)]
+
+
+
+
+
+
+'''    
         if tracker.get_intent_of_latest_message()=="elegir_tema":
             f=open('tierra.txt','r')
         elif tracker.get_intent_of_latest_message()=="aprender_sistemasolar":
@@ -109,7 +141,7 @@ class ActionComprobarRespuesta(Action):
         f.close()'''
 
 
-    '''
+'''
         tema_elegido=tracker.get_slot("tema")
         if tema_elegido.lower() in ALLOWED_TEMAS:
             message:"Has elegido el tema {tema_elegido}."
